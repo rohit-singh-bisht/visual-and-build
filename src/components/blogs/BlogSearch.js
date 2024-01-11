f59d0fa842084cfd905c6824670e6ec1d0d2c376
rohit-singh-bisht <thersbisht@gmail.com>
2024-01-11 22:52:30 +0530

    blog search

---
+++ src/components/blogs/BlogSearch.js
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+import { GoSearch } from "react-icons/go";
+
+const BlogSearchStyle = styled.form`
+  border-radius: 18px;
+  background: #ae0000;
+  padding: 30px;
+  .blog__search__title {
+    color: #fff;
+    font-size: 18px;
+    font-weight: 600;
+    line-height: 22.5px;
+    margin-bottom: 18px;
+  }
+  .blog__search__wrapper {
+    position: relative;
+    .blog__search__input {
+      width: 100%;
+      height: 42px;
+      padding: 15px;
+      border-radius: 7.5px;
+      background: #fff;
+      color: #303030;
+      font-size: 12px;
+      font-weight: 400;
+      line-height: 15px;
+    }
+    .blog__search__icon {
+      font-size: 12px;
+      color: ##303030;
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      right: 15px;
+    }
+  }
+`;
+
+const BlogSearch = ({ title = "Blog Search" }) => {
+  return (
+    <BlogSearchStyle>
+      <h3 className="blog__search__title">{title}</h3>
+      <div className="blog__search__wrapper">
+        <input
+          type="text"
+          className="blog__search__input"
+          placeholder={"Search Article ..."}
+        />
+        <GoSearch className="blog__search__icon" />
+      </div>
+    </BlogSearchStyle>
+  );
+};
+
+export default BlogSearch;
--- src/views/user/Blogs.js
+++ src/views/user/Blogs.js
@@ -2,20 +2,48 @@ import React from "react";
 import styled from "styled-components";
 import BlogCard from "../../components/blogs/BlogCard";
 import categoryDummy from "../../assets/category-dummy.jpg";
+import BlogSearch from "../../components/blogs/BlogSearch";
 
-const BlogsStyle = styled.section``;
+const BlogsStyle = styled.section`
+  .blogs__body {
+    display: flex;
+    gap: 42px;
+    aside {
+      width: 330px;
+    }
+    .blogs__wrapper {
+      flex: 1;
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      row-gap: 12px;
+    }
+  }
+`;
 
 const Blogs = () => {
   return (
     <BlogsStyle>
       <div className="container">
-        <BlogCard
-          blogSrc={categoryDummy}
-          blogTitle={"Sorem ipsum dolor sit amet, consectetur adipiscing elit."}
-          tag={"Tips"}
-          author={"Janet Polly"}
-          date={"12th, April 2023"}
-        />
+        <div className="blogs__body">
+          <aside>
+            <BlogSearch />
+          </aside>
+          <div className="blogs__wrapper">
+            {Array.from({ length: 10 }, (_, index) => index + 1)?.map(
+              (item) => (
+                <BlogCard
+                  blogSrc={categoryDummy}
+                  blogTitle={
+                    "Sorem ipsum dolor sit amet, consectetur adipiscing elit."
+                  }
+                  tag={"Tips"}
+                  author={"Janet Polly"}
+                  date={"12th, April 2023"}
+                />
+              )
+            )}
+          </div>
+        </div>
       </div>
     </BlogsStyle>
   );
