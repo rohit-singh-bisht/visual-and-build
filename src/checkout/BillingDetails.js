5c03b62d57f73c18ddfb8fd9c0dd3012593af319
rohit-singh-bisht <thersbisht@gmail.com>
2024-01-19 09:53:27 +0530

    Billing Details Fix

--- src/checkout/BillingDetails.js
+++ src/checkout/BillingDetails.js
@@ -1,25 +1,61 @@
 import React from "react";
 import styled from "styled-components";
+import { ReactComponent as HelpCircle } from "../assets/help-circle.svg";
+import { ReactComponent as RadioEmpty } from "../assets/radio-empty.svg";
+import { ReactComponent as RadioFilled } from "../assets/radio-filled.svg";
 
 const BillingDetailsStyle = styled.div`
-  border-radius: 4.5px;
-  border: 1.5px solid #d9d9d9;
-  background: #fff;
-  .radio__icon {
-    width: 22px;
-    height: 22px;
-    border-radius: 50%;
-    border: ;
+  .billing__input {
+    position: absolute;
+    opacity: 0;
+    visibility: hidden;
+    &:checked {
+      + .billing__details__wrapper {
+        border: 1.5px solid #ae0000;
+        background: #fff3f3;
+        .billing__radio__icon {
+          .empty {
+            display: none;
+          }
+          .filled {
+            display: block;
+          }
+        }
+      }
+    }
+  }
+  .billing__details__wrapper {
+    border-radius: 4.5px;
+    border: 1.5px solid #d9d9d9;
+    background: #fff;
+    display: inline-flex;
+    align-items: center;
+    padding: 22px;
+    cursor: pointer;
+    gap: 14.25px;
+  }
+  .billing__radio__icon {
+    .empty {
+      display: block;
+    }
+    .filled {
+      display: none;
+    }
   }
   .billing__details {
     .billing__method {
       color: #000;
       font-size: 12px;
       font-weight: 600;
+      margin-bottom: 10.5px;
     }
     .details {
       font-size: 11.25px;
       font-weight: 400;
+      margin-bottom: 6.75px;
+    }
+    .billing__details__phone__title {
+      margin-bottom: 0;
     }
     .billing__details__name,
     .billing__details__phone__title {
@@ -30,6 +66,14 @@ const BillingDetailsStyle = styled.div`
       color: #797979;
     }
   }
+  .distance__from__home {
+    color: #ae0000;
+    font-size: 12px;
+    font-weight: 500;
+    margin-top: 15px;
+    display: flex;
+    gap: 5px;
+  }
 `;
 
 const BillingDetails = ({
@@ -37,24 +81,50 @@ const BillingDetails = ({
   billerName,
   billingAddress,
   billingPhone,
+  billingType,
+  onClick,
+  index,
 }) => {
   return (
-    <BillingDetailsStyle>
-      <div className="billing__radio__icon">
-        <input type="radio" />
-      </div>
-      <div className="billing__details">
-        <div className="billing__method">{billingMethod}</div>
-        <div className="details billing__details__name">{billerName}</div>
-        <div className="details billing__details__address">
-          {billingAddress}
-        </div>
-        <div className="details billing__details__phone__title">
-          Phone number
-        </div>
-        <div className="details billing__details__phone">{billingPhone}</div>
-      </div>
-    </BillingDetailsStyle>
+    <>
+      <BillingDetailsStyle>
+        <input
+          id={"billing__address" + index}
+          onClick={onClick}
+          value={billingType}
+          name="billing__address"
+          type="radio"
+          className="billing__input"
+        />
+        <label
+          className={"billing__details__wrapper"}
+          htmlFor={"billing__address" + index}
+        >
+          <div className="billing__radio__icon">
+            <RadioEmpty className="icon empty" />
+            <RadioFilled className="icon filled" />
+          </div>
+          <div className="billing__details">
+            <div className="billing__method">{billingMethod}</div>
+            <div className="details billing__details__name">{billerName}</div>
+            <div className="details billing__details__address">
+              {billingAddress}
+            </div>
+            <div className="details billing__details__phone__title">
+              Phone number
+            </div>
+            <div className="details billing__details__phone">
+              {billingPhone}
+            </div>
+            {billingType === "pickup" && (
+              <p className="distance__from__home">
+                <HelpCircle /> 5 km Far From Your House
+              </p>
+            )}
+          </div>
+        </label>
+      </BillingDetailsStyle>
+    </>
   );
 };
 
